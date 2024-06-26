0b1a7d30c68f3a5e5f6f5df027a606eedafde93e
seb_icp <sebastien.thuillier38@gmail.com>
2024-06-26 12:37:14 +0100

    remove .dfx folder

--- .dfx/ic/canisters/you/service.did.js
+++
@@ -1,9 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  const Mood = IDL.Text;
-  const UserCanister = IDL.Service({
-    'reboot_dailyCheck' : IDL.Func([Mood], [], []),
-    'reboot_isAlive' : IDL.Func([], [IDL.Bool], ['query']),
-  });
-  return UserCanister;
-};
-export const init = ({ IDL }) => { return [IDL.Text]; };
